7bd9b8651409b2ac1cac65298e2615348bfc8bb6
hamdan266 <sehahh26@gmail.com>
2024-09-14 14:50:48 +0700

    create about section

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,7 +4,7 @@ import { twMerge } from 'tailwind-merge';
 
 export const Card = ({ className, children }: PropsWithChildren<{ className?: string }>) => {
     return (
-        <div className={twMerge("bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 after:pointer-events-none p-6", className)}>
+        <div className={twMerge("bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 after:pointer-events-none", className)}>
             <div
                 className="absolute inset-0 -z-10 opacity-5"
                 style={{
--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -53,7 +53,7 @@ export const TestimonialsSection = () => {
           <div className="flex gap-8 flex-none">
             {testimonials.map((testimonial) => (
 
-              <Card key={testimonial.name} className="max-w-xs md:max-w-md md:p-8">
+              <Card key={testimonial.name} className="max-w-xs md:max-w-md p-6 md:p-8">
                 <div className="flex gap-4 items-center">
                   <div className="size-14 bg-gray-700 inline-flex items-center justify-center rounded-full flex-shrink-0">
                     <Image src={testimonial.avatar} alt={testimonial.name} className="max-h-full" />
