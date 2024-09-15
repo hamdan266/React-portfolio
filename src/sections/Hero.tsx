07fd5584c5738104fff411e56e0cc5651f442eb3
hamdan266 <sehahh26@gmail.com>
2024-09-15 09:24:24 +0700

    create footer section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -59,7 +59,9 @@ export const HeroSection = () => {
         <div className="flex flex-col items-center">
           <Image src={memojiImage} className="size-[100px]" alt="Person peeking from behind laptop" />
           <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
-            <div className="bg-green-500 size-2.5 rounded-full"></div>
+            <div className="bg-green-500 size-2.5 rounded-full relative">
+              <div className="bg-green-500 absolute inset-0 rounded-full animate-ping-large"></div>
+            </div>
             <div className="text-sm font-medium">
               Available for new projects
             </div>
