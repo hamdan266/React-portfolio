d2373e05d850c35854bcb45de9d7a5ec2d24b0bd
hamdan266 <sehahh26@gmail.com>
2024-09-17 02:10:37 +0700

    hero section animation

--- src/components/HeroOrbit.tsx
+++ src/components/HeroOrbit.tsx
@@ -1,22 +1,43 @@
 import { PropsWithChildren } from "react"
+import { twMerge } from "tailwind-merge";
 
 export const HeroOrbit = ({
     children,
     size,
     rotation,
-}: PropsWithChildren<{ size: number; rotation: number; }>) => {
+    shouldOrbit = false,
+    orbitDuration,
+    shouldSpin = false,
+    spinDuration,
+}: PropsWithChildren<{
+    size: number;
+    rotation: number;
+    shouldOrbit?: boolean;
+    shouldSpin?: boolean;
+    spinDuration?: string;
+    orbitDuration?: string;
+}>) => {
     return (
-        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-            <div className="flex items-start justify-start"
+        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 -z-20">
+            <div className={twMerge(shouldOrbit === true && "animate-spin")}
                 style={{
-                    transform: `rotate(${rotation}deg)`,
-                    height: `${size}px`,
-                    width: `${size}px`
+                    animationDuration: orbitDuration,
                 }}>
-                <div className="inline-flex" style={{
-                    transform: `rotate(${rotation * -1}deg)`,
-                }}>
-                    {children}
+                <div className="flex items-start justify-start"
+                    style={{
+                        transform: `rotate(${rotation}deg)`,
+                        height: `${size}px`,
+                        width: `${size}px`
+                    }}>
+                    <div className={twMerge(shouldSpin === true && "animate-spin")} style={{
+                        animationDuration: spinDuration
+                    }}>
+                        <div className="inline-flex" style={{
+                            transform: `rotate(${rotation * -1}deg)`,
+                        }}>
+                            {children}
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
